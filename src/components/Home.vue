d6ef45827a2c8ff175797d51281022f4d8c46f20
Hendra Ramadani <hendraramadani14@gmail.com>
2020-12-03 22:34:43 +0700

    up

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -10,11 +10,20 @@
       <label>Profile name : {{ user.name }}</label> <br>
       <label>Profile bio : {{ user.bio }}</label>
     </div>
+      <info-card
+      id="info-card"
+      frontType="graph"
+      frontTitle="Some Numbers"
+      :frontData="[3, 2, 5, 9, 5, 10, 3, 5, 0, 0, 1, 8, 2, 9, 0]"
+      backTitle="Quote of the Day"
+      backData='<i>Do you know why people like violence? It is because it feels good. Humans find violence deeply satisfying. But remove the satisfaction, and the act becomes hollow.</i><br><span class="u-pull-right">— Alan Turing</span>'
+    ></info-card>
   </div>
 </template>
 
 <script>
   import GithubService from '@/services/GithubService'
+  import InfoCard from 'vue-info-card'
 
   export default {
 
